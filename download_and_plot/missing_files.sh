54459a2a92cf1be83ac68e30d1aea24c414ac9b5
ricampos <ricardo.campos@noaa.gov>
2025-06-03 12:08:52 -0400

    small shell script, to find missing or small GEFSv12 files

--- download_and_plot/missing_files.sh
+++ download_and_plot/missing_files.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# Output file
-missing_file_list="missing_files.txt" > "$missing_file_list"  # Clear file if exists
-
-# Date range
 start_date="20201001"
 end_date="20250331"
 
+min_size=480000000
+
+output_file="missing_or_small_nc_files.txt" > "$output_file"
+
+# Date loop
 current=$(date -d "$start_date" +%s)
 end=$(date -d "$end_date" +%s)
 
-# Loop over each day
 while [ "$current" -le "$end" ]; do
     ymd=$(date -u -d "@$current" +%Y%m%d)
-    
-    # Loop over each ensemble member (00 to 30)
+
     for em in $(seq -w 0 30); do
-        filename="gefs.wave.${ymd}.${em}.global.0p25.nc"
-        
-        if [ ! -f "$filename" ]; then
-            echo "$filename" >> "$missing_file_list"
+        file="gefs.wave.${ymd}.${em}.global.0p25.nc"
+
+        if [ ! -f "$file" ]; then
+            echo "$file" >> "$output_file"
+        else
+            size=$(du -sb "$file" | cut -f1)
+            if [ "$size" -lt "$min_size" ]; then
+                echo "$file" >> "$output_file"
+            fi
         fi
     done
 
-    # Advance to next day
     current=$(( current + 86400 ))
 done
 
-echo "Done. Missing files are listed in $missing_file_list"
+echo "Done. Missing or small .nc files are listed in $output_file"
+
